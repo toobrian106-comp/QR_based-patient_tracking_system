fda72a14db44df7f28bff8ae9f488692098ac674
Brian Kiprotich <your_github_toobrian106@gmail.com>
2026-07-24 23:16:19 +0300

    Connect application to Clever Cloud MySQL

--- config/db.php
+++ config/db.php
@@ -1,6 +1,12 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "patient_tracking_db");
+$host = "bepc4trzsfaa6oaqjouc-mysql.services.clever-cloud.com";
+$user = "uidymb30x4nn7vgm";
+$password = "5UbNzPhmnS8RqJq4VKmm";
+$database = "bepc4trzsfaa6oaqjouc";
+$port = 3306;
+
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
